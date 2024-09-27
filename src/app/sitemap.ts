26bf851dffba4e5f25c35992482dbf5b6494554b
Nguyen Nguyen Khang[khangnguyen] <khangnguyen@nexondv.com>
2024-09-27 14:12:51 +0700

    fix: update site map

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,24 +1,72 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from "next";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://vhgtailorhouse.vn',
+      url: "https://vhgtailorhouse.vn",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: 'https://vhgtailorhouse.vn/products',
+      url: "https://vhgtailorhouse.vn/products/all",
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: 'https://vhgtailorhouse.vn/blogs',
+      url: "https://vhgtailorhouse.vn/products/trouser",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://vhgtailorhouse.vn/products/suit",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://vhgtailorhouse.vn/products/jacket",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://vhgtailorhouse.vn/products/shirt",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://vhgtailorhouse.vn/products/vest",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://vhgtailorhouse.vn/products/blazer",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://vhgtailorhouse.vn/gioi-thieu",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://vhgtailorhouse.vn/blogs",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
       priority: 0.5,
     },
-  ]
+    {
+      url: "https://vhgtailorhouse.vn/quy-trinh",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+  ];
 }
